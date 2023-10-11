f769e85cfffa12688b15defcac949bdb57424bc0
Kohjen <33144900+Kohjen@users.noreply.github.com>
2023-10-11 14:45:38 +1300

    added some functionality to user and moderator

--- backend/src/moderator/moderator.controller.ts
+++ backend/src/moderator/moderator.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from "@nestjs/common";
+import { SubmissionService } from "src/modules/submission/submission.service";
 
-@Controller('moderator')
-export class ModeratorController {}
+@Controller("moderator")
+export class ModeratorController {
+  constructor(private readonly submissionService: SubmissionService) {}
+  /**
+   * get the full list of submissions for moderators
+   * @param response
+   * @returns full list of submissions
+   */
+  @Get("") async getSubmissionList(@Res() response) {
+    try {
+      const submissions = await this.submissionService.findAll();
+      return response.status(HttpStatus.FOUND).json({
+        submissions,
+      });
+    } catch (err) {
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ err });
+    }
+  }
+  /**
+   * get a specific submission
+   * @param response
+   * @param articleID _ID of the submission
+   * @returns the submission of the corrisponding ID
+   */
+  @Get("/:id") async getSubmissionByID(
+    @Res() response,
+    @Param("id") articleID: number,
+  ) {
+    const submission = await this.submissionService.findByID(articleID);
+    return response.status(HttpStatus.OK).json({ submission });
+  }
+}
