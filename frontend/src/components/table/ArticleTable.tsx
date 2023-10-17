557691a41365e03397decb0894229858cb97348e
FilUnderscore <me@filunderscore.com>
2023-10-18 03:55:47 +1300

    Add article table component and add search functionality based off dummy data.

---
+++ frontend/src/components/table/ArticleTable.tsx
@@ -0,0 +1,47 @@
+import SortableTable from "./SortableTable";
+
+export interface ArticlesInterface {
+	id: string;
+	title: string;
+	authors: string;
+	source: string;
+	pubyear: string;
+	doi: string;
+	claim: string;
+	evidence: string;
+}
+
+export type ArticlesProps = {
+	articles: ArticlesInterface[];
+};
+
+const ArticleTable = (props: {articles: ArticlesInterface[]}) => {
+	const { articles } = props;
+	
+	const headers: { key: keyof ArticlesInterface; label: string }[] = [
+		{ key: "title", label: "Title" },
+		{ key: "authors", label: "Authors" },
+		{ key: "source", label: "Source" },
+		{ key: "pubyear", label: "Publication Year" },
+		{ key: "doi", label: "DOI" },
+		{ key: "claim", label: "Claim" },
+		{ key: "evidence", label: "Evidence" },
+	];
+
+	if(articles.length > 0)
+	{
+		return (
+			<div className="container">
+				<SortableTable headers={headers} data={articles}/>
+			</div>
+		);
+	}
+	else
+	{
+		return (
+			<p>No articles found.</p>
+		);
+	}
+};
+
+export default ArticleTable;
--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,44 +1,81 @@
 import { useState } from "react";
 import formStyles from "../styles/Form.module.scss";
+import data from "../utils/dummydata.json"; // TODO: fetch database data.
+import ArticleTable, {ArticlesInterface} from "@/components/table/ArticleTable";
+
 const SearchPage = () => {
-    const [pubYear, setPubYear] = useState<number>(0);
-    return(
-        <div className ="container">
-            <h1>Search for Articles</h1>
-            <form className={formStyles.dropDown}>
-                <label htmlFor="method">Select Method:</label>
-                <select id="methods" name="cars">
-                    <option value="TDD">TDD</option>
-                    <option value="Mob Programming">Mob Programming</option>
-                    <option value="Automated Testing">Automated Testing</option>
-                </select>
+	const currentYear = new Date().getFullYear();
+    const [pubYear, setPubYear] = useState<number>(currentYear);
+	const [filteredArticles, setFilteredArticles] = useState<ArticlesInterface[] | null>(null);
 
-                <label htmlFor="year">Select Publication Year:</label>
-				<input
-					className={formStyles.formItem}
-					type="number"
-					name="pubYear"
-					id="pubYear"
-					value={pubYear}
-					onChange={(event) => {
-						const val = event.target.value;
-						if (val === "") {
-							setPubYear(0);
-						} else {
-							setPubYear(parseInt(val));
-						}
-					}}
-				/>
-                <button className={formStyles.formItem} type="submit">
-					Submit
-				</button>
+	const onSearchFormSubmit = (event: React.FormEvent<HTMLFormElement>) =>
+	{
+		event.preventDefault();
+	    let formData = new FormData(event.currentTarget);
+    	let formObject = Object.fromEntries(formData.entries());
 
-            </form>
+		let method = formObject.method;
+		let pubYear = formObject.pubYear;
+
+		let filteredArticles: ArticlesInterface[] = data.articles.filter((article) => article.pubyear === pubYear).map(article => article as ArticlesInterface);
+		setFilteredArticles(filteredArticles);
+	};
+	
+	if(filteredArticles === null)
+	{
+		return(
+			<div className ="container">
+				<h1>Search for Articles</h1>
+				<form className={formStyles.dropDown} onSubmit={onSearchFormSubmit} action="#">
+					<label htmlFor="method">Select Method:</label>
+					<select id="methods" name="method">
+						<option value="TDD">TDD</option>
+						<option value="Mob Programming">Mob Programming</option>
+						<option value="Automated Testing">Automated Testing</option>
+					</select>
+
+					<label htmlFor="year">Select Publication Year:</label>
+					<input
+						className={formStyles.formItem}
+						type="number"
+						name="pubYear"
+						id="pubYear"
+						value={pubYear}
+						onChange={(event) => {
+							const val = event.target.value;
+							if (val === "") {
+								setPubYear(currentYear);
+							} else {
+								setPubYear(parseInt(val));
+							}
+						}}
+					/>
+					<button className={formStyles.formItem} type="submit">
+						Submit
+					</button>
+
+				</form>
 
 
-        </div>
-    
-    );
+			</div>
+		
+		);
+	}
+	else
+	{
+		return (
+			<div className="container">
+				<h1>Articles</h1>
 
+				<ArticleTable articles={filteredArticles}/>
+				
+				<form className={formStyles.dropDown} onSubmit={() => setFilteredArticles(null)} action="#">
+					<button className={formStyles.formItem} type="submit">
+						Go Back
+					</button>
+				</form>
+			</div>
+		);
+	}
 };
 export default SearchPage;
