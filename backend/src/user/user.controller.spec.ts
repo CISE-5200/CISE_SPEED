7112ace19f80a41b060e84a481281dc6c8b27b9b
Fil <3425556+FilUnderscore@users.noreply.github.com>
2023-10-18 07:07:13 +1300

    Backend & Frontend tests (#10)
    * Update package-lock.json
    
    * Update CI workflows
    
    * Add frontend tests
    
    * Get backend & frontend tests working

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { SubmissionService } from '../modules/submission/submission.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +9,16 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        SubmissionService,
+        {
+          provide: getModelToken('Submission'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<UserController>(UserController);
