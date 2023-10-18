0896f018c83f8d92117a30cede262575bc6c89cd
FilUnderscore <fil@filunderscore.com>
2023-10-18 17:54:08 +1300

    Fix backend

--- backend/src/moderator/moderator.controller.spec.ts
+++ backend/src/moderator/moderator.controller.spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ModeratorController } from './moderator.controller';
+import { SubmissionService } from 'src/modules/submission/submission.service';
 import { getModelToken } from '@nestjs/mongoose';
-import { SubmissionService } from '../modules/submission/submission.service';
+import { RejSubmissionService } from 'src/modules/rejectedSubmissions/rejSubmission.service';
+import { AccSubmissionService } from 'src/modules/acceptedSubmissions/accSubmission.service';
+import { ArticleService } from 'src/modules/Articles/article.service';
 
 describe('ModeratorController', () => {
   let controller: ModeratorController;
@@ -13,6 +16,30 @@ describe('ModeratorController', () => {
         SubmissionService,
         {
           provide: getModelToken('Submission'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+          }
+        },
+        AccSubmissionService,
+        {
+          provide: getModelToken('AccSubmission'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+          }
+        },
+        RejSubmissionService,
+        {
+          provide: getModelToken('RejSubmission'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+          }
+        },
+        ArticleService,
+        {
+          provide: getModelToken('Article'),
           useValue: {
             find: jest.fn(),
             findOne: jest.fn(),
