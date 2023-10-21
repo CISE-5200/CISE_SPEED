b2c6a5ff1657ed4845c85f3110df022da5723b77
Rob Deles <wwj7794@autuni.ac.nz>
2023-10-22 09:56:08 +1300

    added form validation to submit article page

--- frontend/src/pages/articles/new.tsx
+++ frontend/src/pages/articles/new.tsx
@@ -9,7 +9,7 @@ import BACKEND_URL from "@/global";
 const NewDiscussion = () => {
 	const [title, setTitle] = useState("");
 	const [authors, setAuthors] = useState<string[]>([]);
-	const [pubYear, setPubYear] = useState<number>(0);
+	const [year, setYear] = useState<number>(1990);
 	const [journal, setJournal] = useState("");
 	const [method, setMethod] = useState("");
 	const [claim, setClaim] = useState("");
@@ -18,6 +18,70 @@ const NewDiscussion = () => {
 	const [participant, setParticipant] = useState("");
 	const [status, setStatus] = useState("");
 
+	const [errors, setErrors] = useState({
+		title: '',
+		authors: '',
+		year: '',
+		journal: '',
+		claim: '',
+		research: '',
+		participant: '',
+	  });
+
+	const validateForm = () => {
+		const newErrors = {
+		  title: '',
+		  authors: '',
+		  year: '',
+		  journal: '',
+		  claim: '',
+		  research: '',
+		  participant: '',
+		};
+	
+		let isValid = true;
+	
+		if (title.trim() === '') {
+		  newErrors.title = 'Title is required';
+		  isValid = false;
+		}
+	
+		if (authors.some(author => author.trim() === '')) {
+			// Check if any author is empty
+			newErrors.authors ='At least one author is required';
+			isValid = false;
+		}
+	
+		if (year < 1990) {
+		  newErrors.year = 'Publication Year must be earlier than 1990';
+		  isValid = false;
+		}
+	
+		if (journal.trim() === '') {
+		  newErrors.journal = 'Journal is required';
+		  isValid = false;
+		}
+	
+		if (claim.trim() === '') {
+		  newErrors.claim = 'Claim is required';
+		  isValid = false;
+		}
+	
+		if (research.trim() === '') {
+		  newErrors.research = 'Research Type is required';
+		  isValid = false;
+		}
+	
+		if (participant.trim() === '') {
+		  newErrors.participant = 'Research Participant Type is required';
+		  isValid = false;
+		}
+	
+		setErrors(newErrors);
+		return isValid;
+	};
+	
+	
 	const submitNewArticle = async (event: FormEvent<HTMLFormElement>) => {
 		setResult("Unclear");
 		setStatus("Submitted");
@@ -26,7 +90,7 @@ const NewDiscussion = () => {
 			JSON.stringify({
 				title,
 				authors,
-				pubYear,
+				year,
 				journal,
 				method,
 				claim,
@@ -36,21 +100,27 @@ const NewDiscussion = () => {
 			})
 		);
 
-		axios.post(`${BACKEND_URL}/user/submit`, {
-			title: title,
-			authors: authors,
-			year: pubYear,
-			journal: journal,
-			method: method,
-			claim: claim,
-			result: result,
-			researchType: research,
-			participant: participant,
-			status: status,
-
-		}).then((response) => {
-			let data = response.data;
-		});
+		if (validateForm()) {
+			console.log('Form is valid and can be submitted');
+			// Submit data
+			axios.post(`${BACKEND_URL}/user/submit`, {
+				title: title,
+				authors: authors,
+				year: year,
+				journal: journal,
+				method: method,
+				claim: claim,
+				result: result,
+				researchType: research,
+				participant: participant,
+				status: status,
+	
+			}).then((response) => {
+				let data = response.data;
+			});
+		} else {
+			console.log('Form has errors. Please correct them.');
+		}
 	};
 
 	// Some helper methods for the authors array
@@ -83,6 +153,7 @@ const NewDiscussion = () => {
 						setTitle(event.target.value);
 					}}
 				/>
+				<div className={formStyles.error}>{errors.title}</div>
 				<label htmlFor="author">Authors:</label>
 				{authors.map((author, index) => {
 					return (
@@ -97,22 +168,24 @@ const NewDiscussion = () => {
 				<button onClick={() => addAuthor()} className={formStyles.buttonItem} style={{ marginLeft: "auto" }} type="button">
 					+
 				</button>
+				<div className={formStyles.error}>{errors.authors}</div>
 				<label htmlFor="pubYear">Publication Year:</label>
 				<input
 					className={formStyles.formItem}
 					type="number"
 					name="pubYear"
 					id="pubYear"
-					value={pubYear}
+					value={year}
 					onChange={(event) => {
 						const val = event.target.value;
 						if (val === "") {
-							setPubYear(0);
+							setYear(0);
 						} else {
-							setPubYear(parseInt(val));
+							setYear(parseInt(val));
 						}
 					}}
 				/>
+				<div className={formStyles.error}>{errors.year}</div>
 				<label htmlFor="journal">Journal:</label>
 				<input
 					className={formStyles.formItem}
@@ -124,6 +197,7 @@ const NewDiscussion = () => {
 						setJournal(event.target.value);
 					}}
 				/>
+				<div className={formStyles.error}>{errors.journal}</div>
 				<label htmlFor="claim">Claim:</label>
 				<input
 					className={formStyles.formItem}
@@ -135,9 +209,10 @@ const NewDiscussion = () => {
 						setClaim(event.target.value);
 					}}
 				/>
+				<div className={formStyles.error}>{errors.claim}</div>
 				<label htmlFor="method">Select Method:</label>
 				<select id="method" name="method" onChange={(event) => {setMethod(event.target.value)}}>
-					<option value="vTDD">TDD</option>
+					<option value="TDD">TDD</option>
 					<option value="Mob Programming">Mob Progarmming</option>
 					<option value="Automated Testing">Automated Testing</option>
 				</select>
@@ -152,6 +227,7 @@ const NewDiscussion = () => {
 						setResearch(event.target.value);
 					}}
 				/>
+				<div className={formStyles.error}>{errors.research}</div>
 				<label htmlFor="participant">Reseach Participant type:</label>
 				<input
 					className={formStyles.formItem}
@@ -163,6 +239,7 @@ const NewDiscussion = () => {
 						setParticipant(event.target.value);
 					}}
 				/>
+				<div className={formStyles.error}>{errors.participant}</div>
 				<button className={formStyles.formItem} type="submit">
 					Submit
 				</button>
