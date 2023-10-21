0fa8a412ca09be9a03c079bd59784310d0d5fbe7
FilUnderscore <me@filunderscore.com>
2023-10-22 05:03:21 +1300

    Fix articles throwing 404

--- frontend/src/pages/articles/index.tsx
+++ frontend/src/pages/articles/index.tsx
@@ -1,29 +1,20 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import ArticleTable, {ArticlesInterface, ArticlesProps} from "@/components/table/ArticleTable";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import BACKEND_URL from "@/global";
+import { RequestType, useRequest } from "@/lib/auth";
 
 const ArticlesPage: NextPage<ArticlesProps> = () => {
-	const [articleData, setArticleData] = useState<ArticlesInterface[]>();
+	const articlesResponse = useRequest('/article/all', RequestType.GET);
+	const [articles, setArticles] = useState<ArticlesInterface[]>();
 
 	useEffect(() => {
-		getArticleData();
-	}, []);
-
-	const getArticleData = async () => {
-		try {
-			const response = await axios.get(`${BACKEND_URL}/user/list`);
-		  	setArticleData(response.data.articles);
-		} catch (error) {
-		  console.log(error);
-		}
-	  };
+		setArticles(articlesResponse.data?.articles);
+	}, [articlesResponse.data]);
 
 	return (
 		<div className="container">
 			<h1>Articles Index Page</h1>
-			<ArticleTable articles={articleData}/>
+			<ArticleTable articles={articles}/>
 		</div>
 	);
 };
